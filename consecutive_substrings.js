68a87fe743bb2dc2856e5d698114a771f7869732
Ashley Lopez <ashleylopez8976@gmail.com>
2026-09-24 14:43:09 -0500

    Complete consecutive substrings lab

--- consecutive_substrings.js
+++ consecutive_substrings.js
@@ -1,9 +1,19 @@
 function consecutiveSubstrings(string) {
-  // type your code here
+  const result = [];
+
+  for (let start = 0; start < string.length; start++) { 
+    for (let end = start + 1; end <= string.length; end++) {
+      result.push(string.slice(start, end));
+  }
+ }
+
+ return result;
 }
 
 if (require.main === module) {
-  // add your own tests in here
+  console.log("Empty string:", consecutiveSubstrings(""));
+  console.log("Repeated characters:", consecutiveSubstrings("aa"));
+  
   console.log("Expecting: ['a', 'ab', 'abc', 'b', 'bc', 'c']");
   console.log("=>", consecutiveSubstrings('abc'));
 
